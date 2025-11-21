b206dde00f67bde34f222db566ade44cde8e9157
Neizzzy <azat.azatch@yandex.ru>
2025-11-21 18:58:52 +0300

    Add TaskStatus resource route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('task_statuses', TaskStatusController::class)->except(['show']);
