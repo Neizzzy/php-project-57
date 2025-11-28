713d54f365329efd0441eff38d6a2d81c77cefc0
Neizzzy <azat.azatch@yandex.ru>
2025-11-28 15:22:32 +0300

    Remove user_id foreignkey

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -4,16 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TaskStatus extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'user_id'];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = ['name'];
 }
