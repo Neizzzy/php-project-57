56a612ec85a1371b37c358ff38944987e9c39357
Neizzzy <azat.azatch@yandex.ru>
2025-12-03 21:12:31 +0300

    Add label_task pivot migration

---
+++ database/migrations/2025_11_28_203705_create_label_task_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('label_task', function (Blueprint $table) {
+            $table->foreignId('label_id')->constrained()->restrictOnDelete();
+            $table->foreignId('task_id')->constrained()->cascadeOnDelete();
+            $table->primary(['label_id', 'task_id']);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('label_task');
+    }
+};
